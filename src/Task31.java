70b77827581956629a4e6841cfcfa34cbbbd14ae
OksanaDeineka <oksanadeineka06@gmail.com>
2018-08-23 23:31:44 +0300

    unused2

---
+++ src/Task31.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+ public class Task31 {
+    public static void main(String[] args){
+        int aro[]={1,2,45,23,2,12,0,9,10,11};
+        Arrays.sort(aro);
+        for(int v: aro)
+            System.out.println(v);
+    }
+}
