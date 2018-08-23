e587611d9d47758d711d681b15b1add3cd303aef
OksanaDeineka <oksanadeineka06@gmail.com>
2018-08-23 07:33:18 +0300

    unused

---
+++ src/Task21.java
@@ -0,0 +1,12 @@
+public class Task21 {public static void main (String[] args){
+    String s = "ABC";
+    String w = "DFKJ";
+    String k = "abc"+"dfkj";
+    System.out.println(s);
+    System.out.println(w);
+    System.out.println(k);
+    System.out.println(s+k);
+    System.out.println(k+w+s);
+    System.out.println(s+w+k);
+}
+}
