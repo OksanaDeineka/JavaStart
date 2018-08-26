ca83da2d5f10f9f0b4ad58ac2b583ce58a9b6227
OksanaDeineka <oksanadeineka06@gmail.com>
2018-08-26 15:35:56 +0300

    unused2

---
+++ src/Task033.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Task033 {
+    public static void main (String[] args){
+        Scanner ko = new Scanner(System.in);
+        int [] a = new int[4];
+
+        a[0]= ko.nextInt();
+        a[1]= ko.nextInt();
+        a[2]= ko.nextInt();
+        a[3]= ko.nextInt();
+    }
+}
