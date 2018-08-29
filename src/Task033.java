06fcc1165d5552f1bea8eff57959fb5ae175dca8
OksanaDeineka <oksanadeineka06@gmail.com>
2018-08-29 16:26:30 +0300

    unused2

--- src/Task033.java
+++ src/Task033.java
@@ -11,7 +11,18 @@ public class Task033 {
         a[2]= ko.nextInt();
         a[3]= ko.nextInt();
 
-        int len = a.length;
+        int len = a.length;// функция указывает на длину масива= количество элементов
         System.out.println(len);
+
+        //int [] c1 = Arrays.copyOf(a, a.length);
+        //System.out.println(c1);
+
+        int [] c2 = Arrays.copyOf(a,2);// функция которыя выводит первую половину масива
+        String s1 = Arrays.toString(c2);
+        System.out.println(s1);
+
+        int [] c3 = Arrays.copyOfRange(a,2,4);// функция которая выводит вторую половину масива
+        String s2 = Arrays.toString(c3);
+        System.out.println(s2);
     }
 }
