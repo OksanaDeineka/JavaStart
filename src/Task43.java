aeb714503ca3c91d99b296440aaa13ae1b580d46
OksanaDeineka <oksanadeineka06@gmail.com>
2018-09-06 20:52:17 +0300

    unused2

---
+++ src/Task43.java
@@ -0,0 +1,9 @@
+public class Task43 {
+    public static void main(String[] args){
+        int number []= {10,11,12,13,14,15,16,17,18,19,20};
+        //int x;
+        for(int x:number){
+            System.out.println(x);
+        }
+    }
+}
